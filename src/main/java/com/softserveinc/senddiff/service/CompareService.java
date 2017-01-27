5d3f0171d91c74a68960603e65ef1b42c3fa235a
Liubomyr Vypasniak <liubomyr.vypasniak@yourcareuniverse.com>
2017-01-27 19:31:40 +0200

    Added compared object class

--- src/main/java/com/softserveinc/senddiff/service/CompareService.java
+++ src/main/java/com/softserveinc/senddiff/service/CompareService.java
@@ -1,12 +1,13 @@
 package com.softserveinc.senddiff.service;
 
+import com.softserveinc.senddiff.ComparedObject;
 import org.apache.commons.io.FileUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.HashSet;
+import java.util.*;
 
 /**
  * Created by lvypas on 27.01.2017.
@@ -15,11 +16,10 @@ public class CompareService {
 
     private static final Logger logger = LoggerFactory.getLogger(CompareService.class);
 
-    public HashSet<String> compareCsvFiles(String firstFile, String secondFile){
+    public List<ComparedObject> compareCsvFiles(String firstFile, String secondFile){
         HashSet<String> f1 = null;
         HashSet<String> f2 = null;
         HashSet<String> f2copy = null;
-        HashSet<String> resultHS = null;
         try {
             f1 = new HashSet<String>(FileUtils.readLines(new File(firstFile)));
             f2 = new HashSet<String>(FileUtils.readLines(new File(secondFile)));
@@ -29,10 +29,27 @@ public class CompareService {
         }
         f2.removeAll(f1); // f2 now contains only the lines which are not in f1
         f1.removeAll(f2copy); // f1 now contains only the lines which are not in f2
-
         logger.info("Was: " + f2);
         logger.info("Now: " + f1);
-        return resultHS;
+
+        List<ComparedObject> result = new ArrayList<ComparedObject>();
+        Map<Integer, String> oldValues = transformSetToMap(f2);
+        Map<Integer, String> newValues = transformSetToMap(f1);
+        for (Integer entry : oldValues.keySet()) {
+            ComparedObject comparedObject = new ComparedObject(entry, oldValues.get(entry), newValues.get(entry));
+            result.add(comparedObject);
+        }
+        return result;
+    }
+
+    private Map<Integer, String> transformSetToMap(HashSet<String> source) {
+        Iterator<String> iterator = source.iterator();
+        Map<Integer, String> result = new HashMap<Integer, String>();
+        while (iterator.hasNext()) {
+           String line = iterator.next();
+           // TODO: create map fro set
+        }
+        return result;
     }
 
     public void renameFile(String oldName, String newName) throws IOException {
