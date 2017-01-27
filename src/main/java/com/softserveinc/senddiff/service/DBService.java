4a10da293ad13f0e0e9f259c936c9ee10e7f1c4d
Liubomyr Vypasniak <liubomyr.vypasniak@yourcareuniverse.com>
2017-01-27 15:50:38 +0200

    Refactored dbService. added new properties

--- src/main/java/com/softserveinc/senddiff/service/DBService.java
+++ src/main/java/com/softserveinc/senddiff/service/DBService.java
@@ -1,61 +1,65 @@
 package com.softserveinc.senddiff.service;
 
 import org.apache.commons.io.FileUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 import java.sql.*;
 import java.util.HashSet;
+import java.util.Properties;
 
 /**
  * Created by lvypas on 26.01.2017.
  */
 public class DBService {
 
-    PropertiesService propertiesService;
-    Connection connection;
+    private static final Logger logger = LoggerFactory.getLogger(DBService.class);
 
-    public DBService() {
-        propertiesService = new PropertiesService();
-        propertiesService.readProperties();
-    }
-
+    private static final String DATABASE_HOST = "db.database.host";
+    private static final String DATABASE_PORT = "db.database.port";
+    private static final String DATABASE_SCHEME = "db.database.scheme";
+    private static final String DATABASE_USER = "db.database.user";
+    private static final String DATABASE_PASSWORD = "db.database.password";
+    private static final String DATABASE_TABLE = "db.database.table";
+    private static final String FILENAME = "config.properties";
     public static final String MYSQL_JDBC_DRIVER = "com.mysql.jdbc.Driver";
 
-    public Connection connectToDB() {
+    public Properties dbProperties;
+    public Connection connection;
+
+    public void getPropersties() {
+        dbProperties = new Properties();
+        try {
+            dbProperties.load(new FileInputStream(FILENAME));
+        } catch (IOException e) {
+            throw new RuntimeException("Properties file not found");
+        }
+    }
+
+    public void connectToDB() {
         try {
             Class.forName(MYSQL_JDBC_DRIVER);
-        } catch (ClassNotFoundException e) {
-            System.out.println("MySQL JDBC Driver not found");
-            e.printStackTrace();
+        } catch (ClassNotFoundException cnfe) {
+            logger.error("MySQL JDBC Driver not found: " + cnfe.getMessage());
         }
-
-        System.out.println("MySQL JDBC Driver Registered!");
-
+        getPropersties();
         try {
             connection =  DriverManager.getConnection(
-                    "jdbc:mysql://" + propertiesService.databaseUri + ":3306/" + propertiesService.database,
-                    propertiesService.dbuser,
-                    propertiesService.dbpassword);
-
-        } catch (SQLException e) {
-            System.out.println("Connection Failed! Check output console");
-            e.printStackTrace();
+                    "jdbc:mysql://" + dbProperties.getProperty(DATABASE_HOST) +":"+
+                            dbProperties.getProperty(DATABASE_PORT) + "/" + dbProperties.getProperty(DATABASE_SCHEME),
+                    dbProperties.getProperty(DATABASE_USER), dbProperties.getProperty(DATABASE_PASSWORD));
+        } catch (SQLException se) {
+            logger.error("DB connection Failed!: " + se.getMessage());
         }
-
-        if (connection == null) {
-            System.out.println("Failed to make connection!");
-        }
-        return connection;
     }
 
     public void closeConnection(){
-        if(connection !=null){
+        if (connection != null){
             try {
                 connection.close();
             } catch (SQLException ex) {
-                ex.printStackTrace();
+                logger.error("Connection close Failed!: " + ex.getMessage());
             }
         }
 
@@ -64,7 +68,7 @@ public class DBService {
     public void tableToCsv(String fileName) {
         try {
             FileWriter fw = new FileWriter(fileName);
-            String query = "select * from "+ propertiesService.table;
+            String query = "select * from "+ dbProperties.getProperty(DATABASE_TABLE);
             Statement stmt = connection.createStatement();
             ResultSet rs = stmt.executeQuery(query);
             while (rs.next()) {
@@ -79,9 +83,9 @@ public class DBService {
             }
             fw.flush();
             fw.close();
-            System.out.println("CSV File is created successfully.");
-        } catch (Exception e) {
-            e.printStackTrace();
+            logger.info("CSV File is created successfully: ");
+        } catch (Exception ex) {
+            logger.error("File creation failed!: " + ex.getMessage());
         }
     }
 
