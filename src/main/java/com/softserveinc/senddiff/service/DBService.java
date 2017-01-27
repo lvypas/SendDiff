90c541438ebfba2061d3640943cc3495d780e000
Liubomyr Vypasniak <liubomyr.vypasniak@yourcareuniverse.com>
2017-01-27 13:52:52 +0200

    Added comparing two csv files

--- src/main/java/com/softserveinc/senddiff/service/DBService.java
+++ src/main/java/com/softserveinc/senddiff/service/DBService.java
@@ -1,7 +1,12 @@
 package com.softserveinc.senddiff.service;
 
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
 import java.io.FileWriter;
+import java.io.IOException;
 import java.sql.*;
+import java.util.HashSet;
 
 /**
  * Created by lvypas on 26.01.2017.
@@ -78,6 +83,24 @@ public class DBService {
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    public String compareCsvFiles(String firstFile, String secondFile){
+        String diff = "";
+        // using FileUtils to read in the files.
+        HashSet<String> f1 = null;
+        HashSet<String> f2 = null;
+        try {
+            f1 = new HashSet<String>(FileUtils.readLines(new File(firstFile)));
+            f2 = new HashSet<String>(FileUtils.readLines(new File(secondFile)));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        f2.removeAll(f1); // f2 now contains only the lines which are not in f1
+        diff = f2.toString();
+        System.out.println(diff);
+        return diff;
     }
 
 }
